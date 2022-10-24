a24e404ebd1aab32b5bdf0ed10f09cd086e0246e
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2022-10-24 07:24:21 +0000

    kevm-pyk/kevm: compact opcode width calculations

--- kevm-pyk/src/kevm_pyk/kevm.py
+++ kevm-pyk/src/kevm_pyk/kevm.py
@@ -263,6 +263,22 @@ class KEVM(KProve, KRun):
                 if ws_len < 1024 and stack_needed is not None and stack_needed <= ws_len:
                     return mlTop()
 
+        # { true #Equals V1 +Int #widthOp(OP) >=Int V2 }
+        # { true #Equals V1 +Int #widthOp(OP) <Int V2 }
+        widthop_pattern = KApply('_+Int_', [KVariable('V1'), KApply('#widthOp(_)_EVM_Int_OpCode', [KVariable('OP')])])
+        compact_widthop_pattern_1 = mlEqualsTrue(KApply('_>=Int_', [widthop_pattern, KVariable('V2')]))
+        if cwop_match := compact_widthop_pattern_1.match(constraint):
+            num = cwop_match['V1']
+            if type(num) is KToken:
+                new_width = int(num.token) + KEVM.width_op(cwop_match['OP'])
+                constraint = mlEqualsTrue(KApply('_>=Int_', [intToken(new_width), cwop_match['V2']]))
+        compact_widthop_pattern_2 = mlEqualsTrue(KApply('_<Int_', [widthop_pattern, KVariable('V2')]))
+        if cwop_match := compact_widthop_pattern_2.match(constraint):
+            num = cwop_match['V1']
+            if type(num) is KToken:
+                new_width = int(num.token) + KEVM.width_op(cwop_match['OP'])
+                constraint = mlEqualsTrue(KApply('_<Int_', [intToken(new_width), cwop_match['V2']]))
+
         # { true #Equals V1 >=Int #sizeByteArray(V2) }
         concrete_bytearray_size_pattern = mlEqualsTrue(
             KApply('_>=Int_', [KVariable('V1'), KEVM.size_bytearray(KVariable('V2'))])
